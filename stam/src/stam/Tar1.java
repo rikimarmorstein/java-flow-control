4f699ce03eca7690ec4f523a77d5a361f843e40e
Dell <Dell@DESKTOP-JQF0LA8>
2023-03-29 12:00:46 +0300

    update

--- stam/src/stam/Tar1.java
+++ stam/src/stam/Tar1.java
@@ -10,21 +10,7 @@ public class Tar1 {
 		boolean ff = b.contains(a);
 		System.out.println(ff);
 
-//		for (int i = 0; i < b.length();) {
-//			for (int j = 0; j < b.length() - a.length(); j++) {
-//				i = 0;
-//				while (a.charAt(i) == b.charAt(j) && i < a.length()) {
-//					if (i == a.length() - 1) {
-//						System.out.println("true");
-//						return;
-//					}
-//					i++;
-//					j++;
-//				}
-//			}
-//			System.out.println("false");
-//			return;
-//		}
+
 	}
 
 }
