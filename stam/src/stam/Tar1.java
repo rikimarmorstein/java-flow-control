6840e1a28fd7d4064adf378cf3be981344f4ee64
Dell <Dell@DESKTOP-JQF0LA8>
2023-03-29 12:02:57 +0300

    .

--- stam/src/stam/Tar1.java
+++ stam/src/stam/Tar1.java
@@ -3,6 +3,7 @@ package stam;
 public class Tar1 {
 
 	public static void main(String[] args) {
+//
 
 		String a = "adajljklkka";
 		String b = "adaaadajkljklkkaajkljklkkkam";
@@ -10,7 +11,23 @@ public class Tar1 {
 		boolean ff = b.contains(a);
 		System.out.println(ff);
 
+// no contains:
 
+//		for (int i = 0; i < b.length();) {
+//			for (int j = 0; j < b.length() - a.length(); j++) {
+//				i = 0;
+//				while (a.charAt(i) == b.charAt(j) && i < a.length()) {
+//					if (i == a.length() - 1) {
+//						System.out.println("true");
+//						return;
+//					}
+//					i++;
+//					j++;
+//				}
+//			}
+//			System.out.println("false");
+//			return;
+//		}
 	}
 
 }
